63091b0e9b5bfbb9b43613283a3c5078994db1d9
Ian Babrou <ibobrik@gmail.com>
2015-04-10 10:47:19 +0300

    Huge refactoring
    - switched to elastic
    - using single method Run() with context
    - using option functions to configure reindexer
    - reindexer can be restarted
    - logger is now provided by caller
    - total number of docs and current position are logged now

--- cmd/esreindexer/main.go
+++ cmd/esreindexer/main.go
@@ -3,67 +3,87 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"net/url"
+	"os"
 	"strings"
 
-	"github.com/belogik/goes"
 	"github.com/bobrik/esreindexer"
+	"github.com/olivere/elastic"
+	"golang.org/x/net/context"
 )
+
+type esQuery map[string]interface{}
+
+func (q esQuery) Source() interface{} {
+	return q
+}
 
 func main() {
 	src := flag.String("src", "", "source in format http://host:port/index")
 	dst := flag.String("dst", "", "destination in format http://host:port/index")
-	query := flag.String("query", "{}", "query in json format")
+	query := flag.String("query", "{\"match_all\":{}}", "query in json format")
 	pool := flag.Int("pool", 5, "how many indexers to start")
 	pack := flag.Int("pack", 1000, "scrolling size")
 	flag.Parse()
 
-	if *src == "" || *dst == "" {
-		flag.PrintDefaults()
+	q := esQuery(map[string]interface{}{})
+
+	err := json.Unmarshal([]byte(*query), &q)
+	if err != nil {
+		log.Fatalf("query %q is invalid: %s", *query, err)
+	}
+
+	srcES, srcIndex, err := urlToES(*src)
+	if err != nil {
+		log.Fatal("error creating source: ", err)
+	}
+
+	dstES, dstIndex, err := urlToES(*dst)
+	if err != nil {
+		log.Fatal("error creating destination: ", err)
+	}
+
+	r, err := esreindexer.NewReindexer(
+		esreindexer.SetSource(srcES, srcIndex),
+		esreindexer.SetDestination(dstES, dstIndex),
+		esreindexer.SetLogger(log.New(os.Stderr, "", log.LstdFlags)),
+	)
+	if err != nil {
+		log.Fatal("error creating reindexer: ", err)
+	}
+
+	ctx := context.Background()
+	err = r.Run(ctx, *pool, q, "10m", *pack)
+	if err != nil {
+		log.Fatal("error reindexing: ", err)
+	}
+}
+
+func clientFromURL(u *url.URL) (*elastic.Client, error) {
+	return elastic.NewClient(
+		elastic.SetURL(u.Scheme+"://"+u.Host),
+		elastic.SetSniff(false),
+		elastic.SetMaxRetries(2),
+		elastic.SetErrorLog(log.New(os.Stderr, "", log.LstdFlags)),
+	)
+}
+
+func urlToES(s string) (es *elastic.Client, index string, err error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		err = fmt.Errorf("error parsing %q: %s", s, err)
 		return
 	}
 
-	srcURL, err := url.Parse(*src)
-	if err != nil {
-		log.Fatal("error parsing", src, err)
+	if u.Path == "/" || strings.LastIndex(u.Path, "/") != 0 {
+		err = fmt.Errorf("index path is invalid: %q", u)
+		return
 	}
 
-	dstURL, err := url.Parse(*dst)
-	if err != nil {
-		log.Fatal("error parsing", dst, err)
-	}
+	es, err = clientFromURL(u)
+	index = u.Path[1:]
 
-	if srcURL.Path == "/" || dstURL.Path == "/" {
-		log.Fatal("indices are invalid")
-	}
-
-	srcEs := urlToEs(srcURL)
-	dstEs := urlToEs(dstURL)
-
-	q := map[string]interface{}{}
-
-	err = json.Unmarshal([]byte(*query), &q)
-	if err != nil {
-		log.Fatal("query is invalid", err)
-	}
-
-	r := esreindexer.NewReindexer(srcEs, dstEs, srcURL.Path[1:], dstURL.Path[1:], *pool)
-	r.Log = true
-
-	r.Listen()
-
-	// suck pulls data from elasticsearch
-	err = r.Suck(q, "10m", *pack)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// close resources
-	r.Close()
-}
-
-func urlToEs(u *url.URL) *goes.Connection {
-	p := strings.Split(u.Host, ":")
-	return goes.NewConnection(p[0], p[1])
+	return
 }
--- reindexer.go
+++ reindexer.go
@@ -4,154 +4,200 @@ import (
 	"fmt"
 	"log"
 	"sync"
-	"time"
 
-	"github.com/belogik/goes"
+	"github.com/olivere/elastic"
+	"golang.org/x/net/context"
 )
+
+// ReindexerOptionFunc is a function that configures reindexer
+type ReindexerOptionFunc func(*Reindexer) error
 
 // Reindexer represents process of moving data from one index to another
 type Reindexer struct {
-	srcEs *goes.Connection
-	dstEs *goes.Connection
+	srcES *elastic.Client
+	dstES *elastic.Client
 
-	ch chan []goes.Document
-	wg *sync.WaitGroup
+	srcIndex string
+	dstIndex string
 
-	src string
-	dst string
-
-	done chan error
-
-	// Log what's happening into log package
-	Log bool
+	logger *log.Logger
 }
 
-// NewReindexer creates new reindexer with specified connections and indices
-// pool parameter identifies how many parallel indexing requests are allowed
-func NewReindexer(srcEs, dstEs *goes.Connection, src, dst string, pool int) *Reindexer {
-	return &Reindexer{
-		srcEs: srcEs,
-		dstEs: dstEs,
-		ch:    make(chan []goes.Document, pool),
-		wg:    &sync.WaitGroup{},
-		src:   src,
-		dst:   dst,
-		done:  make(chan error, pool),
+// NewReindexer creates new reindexer with specified options
+func NewReindexer(opts ...ReindexerOptionFunc) (*Reindexer, error) {
+	r := &Reindexer{}
+
+	for _, opt := range opts {
+		err := opt(r)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return r, nil
+}
+
+// SetSource sets source elasticsearch client and index
+func SetSource(es *elastic.Client, index string) ReindexerOptionFunc {
+	return func(r *Reindexer) error {
+		r.srcES = es
+		r.srcIndex = index
+		return nil
 	}
 }
 
-// Listen starts listening for data to index
-func (r *Reindexer) Listen() {
-	for i := 0; i < cap(r.ch); i++ {
+// SetDestination sets destination elasticsearch client and index
+func SetDestination(es *elastic.Client, index string) ReindexerOptionFunc {
+	return func(r *Reindexer) error {
+		r.dstES = es
+		r.dstIndex = index
+		return nil
+	}
+}
+
+// SetLogger sets optional logger for reindexer
+func SetLogger(logger *log.Logger) ReindexerOptionFunc {
+	return func(r *Reindexer) error {
+		r.logger = logger
+		return nil
+	}
+}
+
+// Run reindexes data with pool of workers, defined query, timeout and chunk size
+func (r *Reindexer) Run(ctx context.Context, pool int, query elastic.Query, timeout string, size int) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(pool)
+
+	reqCh := make(chan []elastic.BulkableRequest, pool)
+	errCh := make(chan error, 1)
+
+	r.listen(ctx, wg, reqCh, errCh)
+
+	r.suck(ctx, reqCh, errCh, query, timeout, size)
+	close(reqCh)
+
+	wg.Wait()
+
+	select {
+	case err := <-errCh:
+		return err
+	default:
+		return nil
+	}
+}
+
+// listen starts listening for data to index
+func (r *Reindexer) listen(ctx context.Context, wg *sync.WaitGroup, ch <-chan []elastic.BulkableRequest, errCh chan<- error) {
+	for i := 0; i < cap(ch); i++ {
 		r.log(fmt.Sprintf("Started listener #%d", i))
+
 		go func(i int) {
-			for docs := range r.ch {
-				r.log(fmt.Sprintf("Got %d docs to index in listened #%d", len(docs), i))
+			defer wg.Done()
 
-				dstDocs := make([]goes.Document, 0, len(docs))
-				for _, doc := range docs {
-					doc.Index = r.dst
-					dstDocs = append(dstDocs, doc)
-				}
-
-				for try := 1; ; try++ {
-					_, err := r.dstEs.BulkSend(dstDocs)
-					if err == nil {
-						break
+			for {
+				select {
+				case reqs, ok := <-ch:
+					if !ok {
+						return
 					}
 
-					if try <= 8 {
-						log.Println("error indexing, retrying", err)
-						time.Sleep(time.Second * 10)
-						continue
+					r.log(fmt.Sprintf("Got %d docs to index in listener #%d", len(reqs), i))
+
+					b := r.dstES.Bulk().Index(r.dstIndex)
+					for _, req := range reqs {
+						b.Add(req)
 					}
 
+					_, err := b.Do()
 					if err != nil {
-						r.done <- err
+						writeErrorMaybe(errCh, err)
+						return
 					}
+				case <-ctx.Done():
+					writeErrorMaybe(errCh, ctx.Err())
+					r.log(fmt.Sprintf("context for listener #%d is done: %s", i, ctx.Err()))
+					return
 				}
-
-				r.wg.Done()
 			}
 		}(i)
 	}
 }
 
-// Suck starts pulling data out of elasticsearch and does reindexing
-func (r *Reindexer) Suck(query map[string]interface{}, timeout string, size int) error {
-	r.log("starting reindexing")
-	scan, err := r.srcEs.Scan(query, []string{r.src}, []string{}, timeout, size)
+// suck starts pulling data out of elasticsearch and does reindexing
+func (r *Reindexer) suck(ctx context.Context, ch chan<- []elastic.BulkableRequest, errCh chan<- error, query elastic.Query, timeout string, size int) {
+	r.log("Starting reindexing")
+
+	cursor, err := r.srcES.Scan(r.srcIndex).
+		KeepAlive(timeout).
+		Size(size).
+		Query(query).
+		Do()
 	if err != nil {
-		return err
+		writeErrorMaybe(errCh, err)
+		return
 	}
 
-	if scan.Hits.Total == 0 {
-		return err
+	if cursor.TotalHits() == 0 {
+		return
 	}
 
-	r.log(fmt.Sprintf("Found %d docs", scan.Hits.Total))
+	fetched := 0
 
 	for {
-		response, err := r.srcEs.Scroll(scan.ScrollId, timeout)
+		if ctx.Err() != nil {
+			writeErrorMaybe(errCh, ctx.Err())
+			return
+		}
+
+		res, err := cursor.Next()
 		if err != nil {
-			return err
+			writeErrorMaybe(errCh, err)
+			return
 		}
 
-		if len(response.Hits.Hits) == 0 {
-			break
+		if len(res.Hits.Hits) == 0 {
+			return
 		}
 
-		r.log(fmt.Sprintf("Got %d docs from scroll", len(response.Hits.Hits)))
+		fetched += len(res.Hits.Hits)
 
-		r.wg.Add(1)
+		r.log(fmt.Sprintf("Got %d (%d/%d) docs from scroll", len(res.Hits.Hits), fetched, cursor.TotalHits()))
 
-		r.ch <- r.hitsToDocs(response.Hits.Hits)
-
-		select {
-		case err := <-r.done:
-			if err != nil {
-				return err
-			}
-		default:
-			// meh
-		}
+		ch <- r.hitsToDocs(res.Hits.Hits)
 	}
-
-	r.wg.Wait()
-
-	return nil
 }
 
-// Close stops channels after work is done
-func (r *Reindexer) Close() {
-	close(r.ch)
-	close(r.done)
-}
+// hitsToDocs converts *elastic.SearchHit structs to elastic.BulkableRequest for indexing
+func (r *Reindexer) hitsToDocs(hits []*elastic.SearchHit) []elastic.BulkableRequest {
+	requests := make([]elastic.BulkableRequest, len(hits))
 
-// hitsToDocs converts goes.Hit structs to goes.Document structs for reindexing
-func (r *Reindexer) hitsToDocs(hits []goes.Hit) []goes.Document {
-	result := []goes.Document{}
-
-	for _, hit := range hits {
-		doc := goes.Document{
-			BulkCommand: "index",
-			Index:       hit.Index,
-			Type:        hit.Type,
-			Id:          hit.Id,
-			Fields:      hit.Source,
-		}
-
-		result = append(result, doc)
+	for i, hit := range hits {
+		requests[i] = elastic.NewBulkIndexRequest().
+			Type(hit.Type).
+			Id(hit.Id).
+			Doc(*hit.Source)
 	}
 
-	return result
+	return requests
 }
 
 // log writes message to log package if logging is enabled
 func (r *Reindexer) log(message string) {
-	if !r.Log {
+	if r.logger == nil {
 		return
 	}
 
-	log.Println(message)
+	r.logger.Println(message)
+}
+
+// writeErrorMaybe tries to write error to the channel
+// and returns immediately if the channel is full
+func writeErrorMaybe(ch chan<- error, err error) {
+	select {
+	case ch <- err:
+	default:
+	}
 }
